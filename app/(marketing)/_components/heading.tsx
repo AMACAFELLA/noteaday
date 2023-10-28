0fbbe3a6267ec849d9896056e74826f1391e22ce
Angus Macapella <78750276+AMACAFELLA@users.noreply.github.com>
2023-10-28 08:26:32 +0000

    Auth complete

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,14 @@
 "use client"
 
+import { Spinner } from "@/components/spinner"
 import { Button } from "@/components/ui/button"
+import { useConvexAuth } from "convex/react"
 import { ArrowRight } from "lucide-react"
+import Link from "next/link"
 
 export const Heading = () => {
+    const {isAuthenticated, isLoading} = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +17,19 @@ export const Heading = () => {
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 A connect space to organize your ideas, documents, and plans faster and easier.
             </h3>
-            <Button>
-                Enter Noteaday
-                <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated &&!isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Noteaday
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
         </div>
     )
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [ 
+        {
+            domain: "https://legal-yeti-68.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
